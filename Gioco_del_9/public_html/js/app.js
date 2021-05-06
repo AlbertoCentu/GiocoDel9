9d4850f4dfe83b63c4b2a381c57e0d748d521910
AlbertoCentu <77615546+AlbertoCentu@users.noreply.github.com>
2021-05-06 09:50:37 +0200

    Add files via upload

--- Gioco_del_9/public_html/js/app.js
+++ Gioco_del_9/public_html/js/app.js
@@ -1,267 +1,306 @@
-var APP={
+var APP = {
+
+    ls_inizio: ["A", "B", "C", "D", "E", "F", "G", "H", ""],
+
+    //ls_inizio: ["", "", "", "", "", "", "", "", ""],
+
+    //ls_posizioni: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+
+    ls_fine: ["A", "B", "C", "D", "E", "F", "G", "H", ""],
     
-    //ls_inizio: ["A","H","C","G","X","E","F","B","D"],
-    
-    ls_inizio: ["A","B","C","D","E","F","G","X","H"],
-    
-    ls_fine: ["A","B","C","D","E","F","G","H","X"],
-    
-    //ls_posizioni: [4],
-    
+    mossa: 0, 
+
     posVicine: {
-            0: [1, 3], 
-            1: [0, 2, 4],
-            2: [1, 5],
-            3: [0, 4],
-            4: [1, 3, 5, 7],
-            5: [2, 4, 8],
-            6: [3, 7],
-            7: [4, 6, 8],
-            8: [5, 7]
-        },
-    
-    printer: function()
+        0: [1, 3],
+        1: [0, 2, 4],
+        2: [1, 5],
+        3: [0, 4, 6],
+        4: [1, 3, 5, 7],
+        5: [2, 4, 8],
+        6: [3, 7],
+        7: [4, 6, 8],
+        8: [5, 7]
+    },
+
+    printer: function ()
     {
         for (var i = 0; i < 9; i++) {
             $('#but' + i).html(APP.ls_inizio[i]);
         }
-        console.log(APP.ls_inizio[1]);
     },
-    
-    callBack_start : function (event )
+
+    mixer: function (array) {
+
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+
+    },
+
+    callBack_start: function (event)
     {
-       APP.printer();
+        APP.ls_inizio = APP.mixer(APP.ls_inizio);
+        APP.printer();
+        APP.check_fine();
+        APP.mossa = 0;
+        $('#msg').html('');
+        $('#ris').html("");
     },
-    
-    check_fine: function()
+
+    check_fine: function ()
     {
         var esito = true;
-        for (var i = 0; i < APP.ls_inizio.length; i++);
-            if (APP.ls_inizio[i] !== APP.ls_fine[i]){
+        for (var i = 0; i < APP.ls_inizio.length; i++){
+            if (APP.ls_inizio[i] !== APP.ls_fine[i]) {
                 var esito = false;
-                return;
             }
-        
-        if (esito === true){
-            $('#ris').html("Hai vinto!");
+        }
+
+        if (esito === true) {
+            $('#ris').html('Hai vinto!');
         }
     },
-    
-    callBack_change0: function(event)
-    {   
-        var x = "X";
+
+    callBack_change0: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[0];
         var posChange = APP.ls_inizio.indexOf(change);
-                
-        for (var i = 0; i < APP.posVicine[0].length; i++){
-            if (posX == APP.posVicine[0][i]){
+
+        for (var i = 0; i < APP.posVicine[0].length; i++) {
+            if (posX == APP.posVicine[0][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
-        //APP.ls_inizio[posX] = change;
-        //APP.ls_inizio[posChange] = x;
-        
         APP.printer();
- 
+        $('#msg').html('Mossa ' + APP.mossa);
     },
-    
-    callBack_change1: function(event)
-    {   
-        var x = "X";
+
+    callBack_change1: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[1];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[1].length; i++){
-            if (posX == APP.posVicine[1][i]){
+
+        for (var i = 0; i < APP.posVicine[1].length; i++) {
+            if (posX == APP.posVicine[1][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
         APP.printer();
+        $('#msg').html('Mossa ' + APP.mossa);
 
     },
-    
-    callBack_change2: function(event)
-    {   
-        var x = "X";
+
+    callBack_change2: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[2];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[2].length; i++){
-            if (posX == APP.posVicine[2][i]){
+
+        for (var i = 0; i < APP.posVicine[2].length; i++) {
+            if (posX == APP.posVicine[2][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
         APP.printer();
-        
+        $('#msg').html('Mossa ' + APP.mossa);
+
     },
-    
-    callBack_change3: function(event)
-    {   
-        var x = "X";
+
+    callBack_change3: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[3];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[3].length; i++){
-            if (posX == APP.posVicine[3][i]){
+
+        for (var i = 0; i < APP.posVicine[3].length; i++) {
+            if (posX == APP.posVicine[3][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
         APP.printer();
-        
+        $('#msg').html('Mossa ' + APP.mossa);
+
     },
-    
-    callBack_change4: function(event)
-    {   
-        var x = "X";
+
+    callBack_change4: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[4];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[4].length; i++){
-            if (posX == APP.posVicine[4][i]){
+
+        for (var i = 0; i < APP.posVicine[4].length; i++) {
+            if (posX == APP.posVicine[4][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
         APP.printer();
-        
+        $('#msg').html('Mossa ' + APP.mossa);
+
     },
-    
-    callBack_change5: function(event)
-    {   
-        var x = "X";
+
+    callBack_change5: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[5];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[5].length; i++){
-            if (posX == APP.posVicine[5][i]){
+
+        for (var i = 0; i < APP.posVicine[5].length; i++) {
+            if (posX == APP.posVicine[5][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
         APP.printer();
-        
+        $('#msg').html('Mossa ' + APP.mossa);
+
     },
-    
-    callBack_change6: function(event)
-    {   
-        var x = "X";
+
+    callBack_change6: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[6];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[6].length; i++){
-            if (posX == APP.posVicine[6][i]){
+
+        for (var i = 0; i < APP.posVicine[6].length; i++) {
+            if (posX == APP.posVicine[6][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
             }
         }
         
         APP.printer();
-        
+        $('#msg').html('Mossa ' + APP.mossa);
+
     },
-    
-    callBack_change7: function(event)
-    {   
-        var x = "X";
+
+    callBack_change7: function (event)
+    {
+        var x = "";
         var posX = APP.ls_inizio.indexOf(x);
         var change = APP.ls_inizio[7];
         var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[7].length; i++){
-            if (posX == APP.posVicine[7][i]){
+
+        for (var i = 0; i < APP.posVicine[7].length; i++) {
+            if (posX == APP.posVicine[7][i]) {
                 APP.ls_inizio[posX] = change;
                 APP.ls_inizio[posChange] = x;
+                APP.mossa += 1; 
+            }
+        }
+        
+        APP.printer();
+        $('#msg').html('Mossa ' + APP.mossa);
+
+
+    },
+
+    callBack_change8: function (event)
+    {
+        var x = "";
+        var posX = APP.ls_inizio.indexOf(x);
+        var change = APP.ls_inizio[8];
+        var posChange = APP.ls_inizio.indexOf(change);
+
+        for (var i = 0; i < APP.posVicine[8].length; i++) {
+            if (posX == APP.posVicine[8][i]) {
+                APP.ls_inizio[posX] = change;
+                APP.ls_inizio[posChange] = x;
+            APP.mossa += 1; 
             }
         }
         
         APP.printer();
         APP.check_fine();
-        
+        $('#msg').html('Mossa ' + APP.mossa);
+
     },
-    
-    callBack_change8: function(event)
-    {   
-        var x = "X";
-        var posX = APP.ls_inizio.indexOf(x);
-        var change = APP.ls_inizio[8];
-        var posChange = APP.ls_inizio.indexOf(change);
-        
-        for (var i = 0; i < APP.posVicine[8].length; i++){
-            if (posX == APP.posVicine[8][i]){
-                APP.ls_inizio[posX] = change;
-                APP.ls_inizio[posChange] = x;
-            }
-        }
-        
-        APP.printer();
-        
-        $('#posx').html("posizione iniziale di x: " + posX);
-        $('#posc').html("posizione iniziale della lettera " + change + ": " + posChange);
-        
-    },
-   
-/*__________________________________________________________________*/
-     
-    init_callBack_start_Click : function(){
+
+    /*__________________________________________________________________*/
+
+    init_callBack_start_Click: function () {
         $("#start").on('click', APP.callBack_start);
     },
-    
-    init_callBack_change0_Click: function(){
+
+    init_callBack_change0_Click: function () {
         $("#but0").on('click', APP.callBack_change0);
     },
-    
-    init_callBack_change1_Click: function(){
+
+    init_callBack_change1_Click: function () {
         $("#but1").on('click', APP.callBack_change1);
     },
-    
-    init_callBack_change2_Click: function(){
+
+    init_callBack_change2_Click: function () {
         $("#but2").on('click', APP.callBack_change2);
     },
-    
-    init_callBack_change3_Click: function(){
+
+    init_callBack_change3_Click: function () {
         $("#but3").on('click', APP.callBack_change3);
     },
-    
-    init_callBack_change4_Click: function(){
+
+    init_callBack_change4_Click: function () {
         $("#but4").on('click', APP.callBack_change4);
     },
-    
-    init_callBack_change5_Click: function(){
+
+    init_callBack_change5_Click: function () {
         $("#but5").on('click', APP.callBack_change5);
     },
-    
-    init_callBack_change6_Click: function(){
+
+    init_callBack_change6_Click: function () {
         $("#but6").on('click', APP.callBack_change6);
     },
-    
-    init_callBack_change7_Click: function(){
+
+    init_callBack_change7_Click: function () {
         $("#but7").on('click', APP.callBack_change7);
     },
-    
-    init_callBack_change8_Click: function(){
+
+    init_callBack_change8_Click: function () {
         $("#but8").on('click', APP.callBack_change8);
     },
-       
-/*___________________________________________________________________*/
-    
+
+    /*___________________________________________________________________*/
+
     /* Richiama tutte le funzioni di Bind delle callback */
-    init_AllCallback :  function (){
+    init_AllCallback: function () {
         APP.init_callBack_start_Click();
         APP.init_callBack_change0_Click();
         APP.init_callBack_change1_Click();
@@ -272,12 +311,12 @@ var APP={
         APP.init_callBack_change6_Click();
         APP.init_callBack_change7_Click();
         APP.init_callBack_change8_Click();
-    }    
+    }
 };
 
 /*____________________main()__________________________________________*/
 
-$(document).ready( function(){
+$(document).ready(function () {
     // esegue il bind di tutte le callBack
     APP.init_AllCallback();
     // main si ferma in attesa di evento
